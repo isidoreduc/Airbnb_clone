477f86b6f29e76044593053a315f1d2f79f4e9e7
isidoreduc <isidoreducasse79@gmail.com>
2019-08-06 18:27:58 +0200

    header/navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import Header from './shared/Header';
 
 function App() {
 	return (
-		<div className="container">
+		<div>
 			<Header />
 		</div>
 	);
