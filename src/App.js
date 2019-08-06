4566f241e97adfae37a059639685477b2b0808a4
isidoreduc <isidoreducasse79@gmail.com>
2019-08-06 17:12:53 +0200

    sass integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './shared/Header';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
