a934441ffda3aeef366da2d257b00a44ca2363d8
isidoreduc <isidoreducasse79@gmail.com>
2019-08-11 16:20:18 +0200

    connection to mongodb

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,14 @@
+// express server
 const express = require('express');
 const App = express();
 const PORT = process.env.PORT || 5000;
+
+// db connection
+const mongoose = require('mongoose');
+const config = require('./config/dbConnection');
+mongoose.connect(config.DB_URI, {
+	useNewUrlParser: true
+});
 
 App.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 App.get('/rentals', (req, res) => res.json({ sent: true }));
