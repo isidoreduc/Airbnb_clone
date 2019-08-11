e8018454325f654ab50a3bd2ac1131c5fb374839
isidoreduc <isidoreducasse79@gmail.com>
2019-08-11 18:01:38 +0200

    seeded  mongodb

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const App = express();
 const PORT = process.env.PORT || 5000;
 
-// db connection
-const mongoose = require('mongoose');
-const config = require('./config/dbConnection');
-mongoose.connect(config.DB_URI, {
-	useNewUrlParser: true
-});
-
 App.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 App.get('/rentals', (req, res) => res.json({ sent: true }));
+
+// db connection and push data to DB
+const mongoose = require('mongoose');
+const config = require('./config/dbConnection');
+const MockDB = require('./models/mockDB');
+
+mongoose
+	.connect(config.DB_URI, {
+		useNewUrlParser: true
+	})
+	.then(() => new MockDB().seedDB())
+	.catch(err => console.log(err));
